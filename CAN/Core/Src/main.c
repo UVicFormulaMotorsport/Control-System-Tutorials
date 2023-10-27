84069bd453f117a81a194f35c39f406cd2cc37d1
Surface <petergrant@uvic.ca>
2023-10-26 22:26:21 -0700

    It sends a dummy message over CAN2 every second

--- CAN/Core/Src/main.c
+++ CAN/Core/Src/main.c
@@ -40,13 +40,23 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+CAN_HandleTypeDef hcan2;
 
 /* USER CODE BEGIN PV */
 
+
+// We need to define the CAN transmit header
+// & the array that will contain the data to transmit
+// & the mailbox where the data is added to before being sent
+CAN_TxHeaderTypeDef   TxHeader;
+uint8_t               TxData[8];
+uint32_t              TxMailbox;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_CAN2_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -63,6 +73,9 @@ void SystemClock_Config(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
+
+	// The CAN clock must be enabled
+  __HAL_RCC_CAN2_CLK_ENABLE();
 
   /* USER CODE END 1 */
 
@@ -83,6 +96,8 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_CAN2_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -91,6 +106,24 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+
+	  // HAL CAN requires the bits to be in an array
+	  TxData[0] = 0x31; // register address
+	  TxData[1] = 0x12;
+	  TxData[2] = 0x34;
+
+	  /* Start the Transmission process */
+	  // By using the HAL_CAN_ADD_TxMessage(args), a CAN message will try to be sent
+	  if (HAL_CAN_AddTxMessage(&hcan2, &TxHeader, TxData, &TxMailbox) != HAL_OK)
+	  {
+		/* Transmission request Error */
+		Error_Handler();
+	  }
+
+	  // Blink a light to show a message has been sent
+	  HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_14);
+	  HAL_Delay(1000);
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -139,6 +172,97 @@ void SystemClock_Config(void)
   }
 }
 
+/**
+  * @brief CAN2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_CAN2_Init(void)
+{
+
+  /* USER CODE BEGIN CAN2_Init 0 */
+	// Configure the GPIO pins for CAN1
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+	GPIO_InitStruct.Pin = GPIO_PIN_13 | GPIO_PIN_12;
+	GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+	GPIO_InitStruct.Alternate = GPIO_AF9_CAN1;
+  /* USER CODE END CAN2_Init 0 */
+
+  /* USER CODE BEGIN CAN2_Init 1 */
+
+	// We need to initialize the pins
+	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  /* USER CODE END CAN2_Init 1 */
+  hcan2.Instance = CAN2;
+  hcan2.Init.Prescaler = 2;
+  hcan2.Init.Mode = CAN_MODE_NORMAL;
+  hcan2.Init.SyncJumpWidth = CAN_SJW_1TQ;
+  hcan2.Init.TimeSeg1 = CAN_BS1_13TQ;
+  hcan2.Init.TimeSeg2 = CAN_BS2_2TQ;
+  hcan2.Init.TimeTriggeredMode = DISABLE;
+  hcan2.Init.AutoBusOff = DISABLE;
+  hcan2.Init.AutoWakeUp = DISABLE;
+  hcan2.Init.AutoRetransmission = DISABLE;
+  hcan2.Init.ReceiveFifoLocked = DISABLE;
+  hcan2.Init.TransmitFifoPriority = DISABLE;
+  if (HAL_CAN_Init(&hcan2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN CAN2_Init 2 */
+
+  	/*##-2- Start the CAN peripheral ###########################################*/
+
+  	  // If HAL_CAN_Start returns an error, then we want to go into the error handler
+    if (HAL_CAN_Start(&hcan2) != HAL_OK)
+  	{
+       /* Start Error */
+       Error_Handler();
+  	}
+  	//Configure transmission process
+    // This is where we set the ID and data length and other misc. CAN bits
+  	TxHeader.StdId = 0x201;
+  	TxHeader.ExtId = 0x01;
+  	TxHeader.RTR = CAN_RTR_DATA;
+  	TxHeader.IDE = CAN_ID_STD;
+  	TxHeader.DLC = 3;
+  	TxHeader.TransmitGlobalTime = DISABLE;
+
+  /* USER CODE END CAN2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14|GPIO_PIN_15, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PD14 PD15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_14|GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
 /* USER CODE BEGIN 4 */
 
 /* USER CODE END 4 */
@@ -152,6 +276,7 @@ void Error_Handler(void)
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);
   while (1)
   {
   }
